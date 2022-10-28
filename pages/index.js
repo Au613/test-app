a2c8873b77190deda1b1e0dfd71b23243ed8f28a
Austin <austinmit613@gmail.com>
2022-10-28 15:13:09 -0600

    test first commit

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,9 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
+        <p className="bg-yellow-200 border-blue-800 border-4 rounded-lg">
+          Get started by editing
+        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
---
+++ pages/second.js
@@ -0,0 +1,9 @@
+// import styles from '../styles/Globals.module.css'
+
+export default function Second() {
+  return (
+    <>
+        <h1 className="bg-red-200 border-x-4 border-y-8 border-black">Hello World</h1>
+    </>
+  )
+}
